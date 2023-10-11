0115d23515380e307faa2985df8f4828e90e9869
zsribna <zsribna@gmail.com>
2023-10-11 11:12:50 +0300

    update first cod

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,16 @@
-import user from './Data/user.json';
-import data from './Data/data.json';
-import friends from './Data/friends.json';
-import transactions from './Data/transactions.json';
-
-import Profile from './Profile/profile';
-import Statistics from './Statistics/statistics';
-import FriendList from './FriendList/FriendList';
-import Transactions from './Transactions/Transactions';
-
 export const App = () => {
   return (
-    <>
-      <Profile users={user} />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <Transactions items={transactions} />
-    </>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
 };
